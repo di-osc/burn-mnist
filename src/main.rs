b6b4e98ac54cf2a8a471f0af33c5b56a3ac1ccce
wangmengdi <790990241@qq.com>
2025-05-23 08:48:06 +0800

    训练推理代码

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
+#![recursion_limit = "131"]
+mod data;
+mod inference;
 mod model;
+mod training;
 
 use crate::model::ModelConfig;
-use burn::backend::Wgpu;
+use crate::training::TrainingConfig;
+
+use burn::backend::{Autodiff, Wgpu};
+use burn::data::dataset::Dataset;
+use burn::optim::AdamConfig;
 
 fn main() {
     type MyBackend = Wgpu<f32, i32>;
+    type MyAotudiffBackend = Autodiff<MyBackend>;
 
-    let device = Default::default();
+    let device = burn::backend::wgpu::WgpuDevice::default();
 
-    let model = ModelConfig::new(10, 512).init::<MyBackend>(&device);
-
-    println!("{:?}", model);
+    let artifact_dir = "checkpoints";
+    let start = std::time::Instant::now();
+    training::train::<MyAotudiffBackend>(
+        artifact_dir,
+        TrainingConfig::new(ModelConfig::new(10, 512), AdamConfig::new()),
+        device.clone(),
+    );
+    let duration = start.elapsed();
+    println!("Training time: {:?}", duration);
+    crate::inference::infer::<MyBackend>(
+        artifact_dir,
+        device,
+        burn::data::dataset::vision::MnistDataset::test()
+            .get(40)
+            .unwrap(),
+    );
 }
--- src/model.rs
+++ src/model.rs
@@ -5,9 +5,17 @@ use burn::nn::LinearConfig;
 use burn::nn::Relu;
 use burn::nn::conv::Conv2d;
 use burn::nn::conv::Conv2dConfig;
+use burn::nn::loss::CrossEntropyLossConfig;
 use burn::nn::pool::AdaptiveAvgPool2d;
 use burn::nn::pool::AdaptiveAvgPool2dConfig;
 use burn::prelude::*;
+use burn::tensor::backend::AutodiffBackend;
+use burn::train::ClassificationOutput;
+use burn::train::TrainOutput;
+use burn::train::TrainStep;
+use burn::train::ValidStep;
+
+use crate::data::MnistBatch;
 
 #[derive(Module, Debug)]
 pub struct Model<B: Backend> {
@@ -30,7 +38,7 @@ impl<B: Backend> Model<B> {
         let x = self.conv2.forward(x);
         let x = self.dropout.forward(x);
         let x = self.activation.forward(x);
-
+        // x shape
         let x = self.pool.forward(x);
         let x = x.reshape([batch_size, 16 * 8 * 8]);
         let x = self.linear1.forward(x);
@@ -38,6 +46,32 @@ impl<B: Backend> Model<B> {
         let x = self.activation.forward(x);
         let x = self.linear2.forward(x);
         x
+    }
+
+    pub fn forward_classification(
+        &self,
+        images: Tensor<B, 3>,
+        targets: Tensor<B, 1, Int>,
+    ) -> ClassificationOutput<B> {
+        let output = self.forward(images);
+        let loss = CrossEntropyLossConfig::new()
+            .init(&output.device())
+            .forward(output.clone(), targets.clone());
+
+        ClassificationOutput::new(loss, output, targets)
+    }
+}
+
+impl<B: AutodiffBackend> TrainStep<MnistBatch<B>, ClassificationOutput<B>> for Model<B> {
+    fn step(&self, batch: MnistBatch<B>) -> TrainOutput<ClassificationOutput<B>> {
+        let item = self.forward_classification(batch.images, batch.targets);
+        TrainOutput::new(self, item.loss.backward(), item)
+    }
+}
+
+impl<B: Backend> ValidStep<MnistBatch<B>, ClassificationOutput<B>> for Model<B> {
+    fn step(&self, item: MnistBatch<B>) -> ClassificationOutput<B> {
+        self.forward_classification(item.images, item.targets)
     }
 }
 
