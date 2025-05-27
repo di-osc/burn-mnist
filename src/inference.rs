3aa894a9416803bb7e6bc9e1e19564bc735e04fe
wangmengdi06 <wangmengdi06@58.com>
2025-05-27 17:45:35 +0800

    批次推理

--- src/inference.rs
+++ src/inference.rs
@@ -4,7 +4,7 @@ use burn::data::dataloader::batcher::Batcher;
 use burn::record::Recorder;
 use burn::{data::dataset::vision::MnistItem, prelude::*, record::CompactRecorder};
 
-pub fn infer<B: Backend>(artifact_dir: &str, device: B::Device, item: MnistItem) {
+pub fn infer<B: Backend>(artifact_dir: &str, device: B::Device, items: Vec<MnistItem>) {
     let config = TrainingConfig::load(format!("{artifact_dir}/config.json"))
         .expect("Config should exist for the model; run train first");
     let record = CompactRecorder::new();
@@ -14,11 +14,12 @@ pub fn infer<B: Backend>(artifact_dir: &str, device: B::Device, item: MnistItem)
 
     let model = config.model.init::<B>(&device).load_record(record);
 
-    let label = item.label;
+    let labels = items.iter().map(|item| item.label).collect::<Vec<_>>();
     let batcher = MnistBatcher::default();
-    let batch = batcher.batch(vec![item], &device);
+    let batch = batcher.batch(items, &device);
     let output = model.forward(batch.images);
-    let predicted = output.argmax(1).flatten::<1>(0, 1).into_scalar();
+    let predicted = output.argmax(1).flatten::<1>(0, 1).to_data();
 
-    println!("Predicted {} Expected {}", predicted, label);
+    println!("Predicted: {}", predicted);
+    println!("Labels: {:?}", labels);
 }
